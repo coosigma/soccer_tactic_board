f84af0bb1e064281fd676b4e7a3f0170d3d017cc
coos <iceliang@gmail.com>
2018-10-15 15:55:08 +1300

    添加项目文件。

---
+++ SoccerTacticBoard/FilterViewForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SoccerTacticBoard
+{
+    partial class FilterViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FilterViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 480);
+            this.Name = "FilterViewForm";
+            this.Text = "FilterViewForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
